9aeb6b70da060ed7fa725de42b529703108e3760
wansho <wanshojs@hotmail.com>
2021-05-16 19:53:02 +0800

    feat: init，配置 lombok, JUnit5

---
+++ src/test/java/JUnit5Test.java
@@ -0,0 +1,15 @@
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author wanshuo
+ * @date 2021-05-16 18:55:42
+ */
+@Slf4j
+public class JUnit5Test {
+
+    @Test
+    public void test(){
+        System.out.println("test");
+    }
+}
